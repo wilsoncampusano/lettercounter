43e9268ad8042580ee258f91e98a4c98a2892587
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-02-05 20:02:02 -0400

    silly comments and unused imports

--- src/main/java/LetterCounter.java
+++ src/main/java/LetterCounter.java
@@ -3,9 +3,6 @@ import java.util.List;
 
 import static java.util.Objects.isNull;
 
-/**
- * Created by wilsoncampusano on 2/4/16.
- */
 public class LetterCounter {
     private String sentence;
     List<String> lettersFound;
--- src/test/java/LetterCounterTest.java
+++ src/test/java/LetterCounterTest.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LetterCounterTest {
